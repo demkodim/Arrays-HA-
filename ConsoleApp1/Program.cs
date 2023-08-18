81752af30c03d70b9a9ce26e304a7c8634f287ba
Dmytro Demko <dmytro.d2@gso.schule.koeln>
2023-08-18 08:37:11 +0200

    Projektdateien hinzufügen.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {/*
+            Aufgabe 1.1
+            int[] alter = new int[5];
+            alter[0] = 25;
+            alter[1] = 40;
+            alter[2] = 18;
+            alter[3] = 30;
+            alter[4] = 35;
+            Console.WriteLine(alter[0]);
+            Console.WriteLine(alter[1]);
+            Console.WriteLine(alter[2]);
+            Console.WriteLine(alter[3]);
+            Console.WriteLine(alter[4]);
+            Console.ReadLine();
+
+            Aufgabe 1.2
+            
+            int i = -1;
+            string[] name = new string[5];
+            int[] alter = new int[5];
+            do
+            {
+
+                name[0] = "Alice";
+                name[1] = "Bob";
+                name[2] = "Charlie";
+                name[3] = "David";
+                name[4] = "Eva";
+
+
+                alter[0] = 25;
+                alter[1] = 40;
+                alter[2] = 18;
+                alter[3] = 30;
+                alter[4] = 35;
+                i++;
+                Console.WriteLine($"{name[i]}  {alter[i]} ");
+            }
+            while (i <= alter.Length);
+            
+
+            Aufgabe 2
+            */
+
+
+        }
+    }
+}
